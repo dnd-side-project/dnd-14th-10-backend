9ac859aacfde6cb12255542a0962d773ada886ea
tjrwls1211 <seokjin6635@gmail.com>
2026-01-24 17:03:32 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/io/dnd/goyo/GoyoApplication.java
@@ -0,0 +1,13 @@
+package io.dnd.goyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/dnd/goyo/GoyoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dnd.goyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
