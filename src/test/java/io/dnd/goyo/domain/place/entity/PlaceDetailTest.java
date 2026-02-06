ae487b4bd1570e6afa766aa83191bc43600bab5c
tjrwls1211 <seokjin6635@gmail.com>
2026-02-06 23:06:49 +0900

    refactor: 장소 상세 점수 체계 변경

--- src/test/java/io/dnd/goyo/domain/place/entity/PlaceDetailTest.java
+++ src/test/java/io/dnd/goyo/domain/place/entity/PlaceDetailTest.java
@@ -3,10 +3,8 @@ package io.dnd.goyo.domain.place.entity;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
+
 import io.dnd.goyo.common.exception.BusinessException;
-import io.dnd.goyo.domain.place.enums.CrowdStatus;
-import io.dnd.goyo.domain.place.enums.OutletScore;
-import io.dnd.goyo.domain.place.enums.SpaceSize;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -25,78 +23,78 @@ class PlaceDetailTest {
             Place place = null;
 
             // when & then
-            assertThatThrownBy(() -> PlaceDetail.of(place, 3.5, OutletScore.MANY, CrowdStatus.NORMAL, SpaceSize.MEDIUM))
+            assertThatThrownBy(() -> PlaceDetail.of(place, 50, 50, 50, 50))
                     .isInstanceOf(BusinessException.class)
                     .hasMessageContaining("장소 정보는 필수입니다");
         }
 
         @ParameterizedTest
-        @ValueSource(doubles = {-0.1, -1.0, 5.1, 10.0})
-        void 평점이_범위를_벗어나면_예외_발생(double rating) {
+        @ValueSource(ints = {-1, 101})
+        void 콘센트_점수가_범위를_벗어나면_예외_발생(int score) {
             // given
             Place place = mock(Place.class);
 
             // when & then
-            assertThatThrownBy(() -> PlaceDetail.of(place, rating, OutletScore.MANY, CrowdStatus.NORMAL, SpaceSize.MEDIUM))
+            assertThatThrownBy(() -> PlaceDetail.of(place, score, 50, 50, 50))
                     .isInstanceOf(BusinessException.class)
-                    .hasMessageContaining("평점은 0.0 ~ 5.0 사이여야 합니다");
+                    .hasMessageContaining("콘센트 점수는 0~100 사이여야 합니다");
         }
 
         @ParameterizedTest
-        @ValueSource(doubles = {0.0, 5.0})
-        void 평점_경계값은_허용(double rating) {
+        @ValueSource(ints = {-1, 101})
+        void 혼잡도_점수가_범위를_벗어나면_예외_발생(int score) {
             // given
             Place place = mock(Place.class);
+
+            // when & then
+            assertThatThrownBy(() -> PlaceDetail.of(place, 50, score, 50, 50))
+                    .isInstanceOf(BusinessException.class)
+                    .hasMessageContaining("혼잡도 점수는 0~100 사이여야 합니다");
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {-1, 101})
+        void 공간_크기_점수가_범위를_벗어나면_예외_발생(int score) {
+            // given
+            Place place = mock(Place.class);
+
+            // when & then
+            assertThatThrownBy(() -> PlaceDetail.of(place, 50, 50, score, 50))
+                    .isInstanceOf(BusinessException.class)
+                    .hasMessageContaining("공간 크기 점수는 0~100 사이여야 합니다");
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {-1, 101})
+        void 분위기_점수가_범위를_벗어나면_예외_발생(int score) {
+            // given
+            Place place = mock(Place.class);
+
+            // when & then
+            assertThatThrownBy(() -> PlaceDetail.of(place, 50, 50, 50, score))
+                    .isInstanceOf(BusinessException.class)
+                    .hasMessageContaining("분위기 점수는 0~100 사이여야 합니다");
+        }
+
+        @Test
+        void 초기_값들이_정상적으로_설정된다() {
+            // given
+            Place place = mock(Place.class);
+            int outletScore = 25;
+            int crowdScore = 50;
+            int spaceSizeScore = 75;
+            int quietScore = 100;
 
             // when
-            PlaceDetail placeDetail = PlaceDetail.of(place, rating, OutletScore.MANY, CrowdStatus.NORMAL, SpaceSize.MEDIUM);
+            PlaceDetail placeDetail = PlaceDetail.of(place, outletScore, crowdScore, spaceSizeScore, quietScore);
 
             // then
-            assertThat(placeDetail.getRating()).isEqualTo(rating);
-        }
-
-        @Test
-        void 콘센트_점수가_null이면_예외_발생() {
-            // given
-            Place place = mock(Place.class);
-
-            // when & then
-            assertThatThrownBy(() -> PlaceDetail.of(place, 3.5, null, CrowdStatus.NORMAL, SpaceSize.MEDIUM))
-                    .isInstanceOf(BusinessException.class)
-                    .hasMessageContaining("콘센트 점수는 필수입니다");
-        }
-
-        @Test
-        void 혼잡도가_null이면_예외_발생() {
-            // given
-            Place place = mock(Place.class);
-
-            // when & then
-            assertThatThrownBy(() -> PlaceDetail.of(place, 3.5, OutletScore.MANY, null, SpaceSize.MEDIUM))
-                    .isInstanceOf(BusinessException.class)
-                    .hasMessageContaining("혼잡도는 필수입니다");
-        }
-
-        @Test
-        void 공간_크기가_null이면_예외가_발생() {
-            // given
-            Place place = mock(Place.class);
-
-            // when & then
-            assertThatThrownBy(() -> PlaceDetail.of(place, 3.5, OutletScore.MANY, CrowdStatus.NORMAL, null))
-                    .isInstanceOf(BusinessException.class)
-                    .hasMessageContaining("공간 크기는 필수입니다");
-        }
-
-        @Test
-        void wishCount는_0으로_초기화() {
-            // given
-            Place place = mock(Place.class);
-
-            // when
-            PlaceDetail placeDetail = PlaceDetail.of(place, 3.5, OutletScore.MANY, CrowdStatus.NORMAL, SpaceSize.MEDIUM);
-
-            // then
+            assertThat(placeDetail.getTotalOutletScore()).isEqualTo(outletScore);
+            assertThat(placeDetail.getTotalCrowdScore()).isEqualTo(crowdScore);
+            assertThat(placeDetail.getTotalSpaceSizeScore()).isEqualTo(spaceSizeScore);
+            assertThat(placeDetail.getTotalQuietScore()).isEqualTo(quietScore);
+            assertThat(placeDetail.getTotalRating()).isEqualTo(0.0);
+            assertThat(placeDetail.getReviewCount()).isEqualTo(0);
             assertThat(placeDetail.getWishCount()).isEqualTo(0);
         }
     }
