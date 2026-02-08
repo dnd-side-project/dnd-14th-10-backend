b78cb513446889e2fb6a09e183f4b4fa0d9821e9
tjrwls1211 <seokjin6635@gmail.com>
2026-02-08 18:47:09 +0900

    refactor: 좌표계 SRID 매직넘버 상수화

--- src/main/java/io/dnd/goyo/config/JtsConfig.java
+++ src/main/java/io/dnd/goyo/config/JtsConfig.java
@@ -8,8 +8,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JtsConfig {
 
+    // GPS 표준 좌표계(WGS84)의 SRID
+    private static final int SRID_WGS84 = 4326;
+
     @Bean
     public GeometryFactory geometryFactory() {
-        return new GeometryFactory(new PrecisionModel(), 4326);
+        return new GeometryFactory(new PrecisionModel(), SRID_WGS84);
     }
 }
