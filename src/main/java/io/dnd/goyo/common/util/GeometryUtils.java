116010779cf70c8054a0e9753c722d18c6bc7bff
tjrwls1211 <seokjin6635@gmail.com>
2026-02-12 16:49:30 +0900

    feat: Haversine 거리 계산 메서드 구현

--- src/main/java/io/dnd/goyo/common/util/GeometryUtils.java
+++ src/main/java/io/dnd/goyo/common/util/GeometryUtils.java
@@ -10,9 +10,25 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class GeometryUtils {
 
+    private static final int EARTH_RADIUS_KM = 6371;
+
     private final GeometryFactory geometryFactory;
 
     public Point createPoint(double longitude, double latitude) {
         return geometryFactory.createPoint(new Coordinate(longitude, latitude));
+    }
+
+    public double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        // 미터 단위 반환
+        return EARTH_RADIUS_KM * c * 1000;
     }
 }
