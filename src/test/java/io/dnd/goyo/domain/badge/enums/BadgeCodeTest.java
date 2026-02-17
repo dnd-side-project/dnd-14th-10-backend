fb9446be7a78dd230eee5ae0a7f762e7cd38efc5
김훈민/재무서비스개발1파트/ND <gnsals0904@naver.com>
2026-02-17 23:27:57 +0900

    test: 중복된 display name 제거

--- src/test/java/io/dnd/goyo/domain/badge/enums/BadgeCodeTest.java
+++ src/test/java/io/dnd/goyo/domain/badge/enums/BadgeCodeTest.java
@@ -45,7 +45,6 @@ class BadgeCodeTest {
     }
 
     @Test
-    @DisplayName("모든 뱃지 코드에 displayName이 존재")
     void 모든_뱃지_코드에_displayName이_존재() {
         for (BadgeCode badgeCode : BadgeCode.values()) {
             assertThat(badgeCode.getDisplayName()).isNotBlank();
@@ -53,7 +52,6 @@ class BadgeCodeTest {
     }
 
     @Test
-    @DisplayName("REVIEW 타입 뱃지의 displayName 검증")
     void REVIEW_타입_뱃지의_displayName_검증() {
         // when
         List<BadgeCode> reviewBadges = BadgeCode.getByActivityType(ActivityType.REVIEW);
@@ -64,7 +62,6 @@ class BadgeCodeTest {
     }
 
     @Test
-    @DisplayName("동일한 캐시 인스턴스를 반환")
     void 동일한_캐시_인스턴스를_반환() {
         // when
         List<BadgeCode> first = BadgeCode.getByActivityType(ActivityType.REVIEW);
