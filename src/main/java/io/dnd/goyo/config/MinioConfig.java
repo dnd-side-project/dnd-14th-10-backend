a16e3b4e3828c30cd5ce251bca7a480abf409c52
김훈민/재무서비스개발1파트/ND <gnsals0904@naver.com>
2026-02-22 23:07:38 +0900

    fix: 이미지 업로드 기능 버그 픽스
    - presigned / public url 방식 동작하도록 수정
    - configuration properties로 설정 관리 하도록 수정

--- src/main/java/io/dnd/goyo/config/MinioConfig.java
+++ src/main/java/io/dnd/goyo/config/MinioConfig.java
@@ -1,27 +1,21 @@
 package io.dnd.goyo.config;
 
 import io.minio.MinioClient;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class MinioConfig {
 
-    @Value("${minio.endpoint}")
-    private String endpoint;
-
-    @Value("${minio.access-key}")
-    private String accessKey;
-
-    @Value("${minio.secret-key}")
-    private String secretKey;
+    private final MinioProperties minioProperties;
 
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
-                .endpoint(endpoint)
-                .credentials(accessKey, secretKey)
+                .endpoint(minioProperties.endpoint())
+                .credentials(minioProperties.accessKey(), minioProperties.secretKey())
                 .build();
     }
 }
