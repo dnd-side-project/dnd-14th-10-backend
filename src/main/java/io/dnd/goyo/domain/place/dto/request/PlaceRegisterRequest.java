23f040f603cca8a817aa0793a3d7ac014f484da1
tjrwls1211 <seokjin6635@gmail.com>
2026-02-22 16:12:57 +0900

    refactor: 중복 검증 어노테이션 추가

--- src/main/java/io/dnd/goyo/domain/place/dto/request/PlaceRegisterRequest.java
+++ src/main/java/io/dnd/goyo/domain/place/dto/request/PlaceRegisterRequest.java
@@ -11,6 +11,7 @@ import io.dnd.goyo.domain.place.enums.PlaceCategory;
 import io.dnd.goyo.domain.place.enums.SpaceSize;
 import io.dnd.goyo.domain.user.entity.User;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.dnd.goyo.common.validation.NoDuplicates;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -80,6 +81,7 @@ public record PlaceRegisterRequest(
         @Schema(description = "태그 ID 리스트", example = "[1, 5, 12]")
         @NotNull(message = "태그는 필수입니다")
         @Size(min = 2, max = 5, message = "태그는 2~5개 선택해야 합니다")
+        @NoDuplicates(message = "태그 ID는 중복될 수 없습니다")
         List<Long> tagIds,
 
         @Schema(description = "업로드된 사진 리스트")
--- src/main/java/io/dnd/goyo/domain/place/dto/request/PlaceUpdateRequest.java
+++ src/main/java/io/dnd/goyo/domain/place/dto/request/PlaceUpdateRequest.java
@@ -6,6 +6,7 @@ import io.dnd.goyo.domain.place.enums.Mood;
 import io.dnd.goyo.domain.place.enums.OutletScore;
 import io.dnd.goyo.domain.place.enums.SpaceSize;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.dnd.goyo.common.validation.NoDuplicates;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -47,6 +48,7 @@ public record PlaceUpdateRequest(
         @Schema(description = "태그 ID 리스트", example = "[1, 5, 12]")
         @NotNull(message = "태그는 필수입니다")
         @Size(min = 2, max = 5, message = "태그는 2~5개 선택해야 합니다")
+        @NoDuplicates(message = "태그 ID는 중복될 수 없습니다")
         List<Long> tagIds,
 
         @Schema(description = "업로드된 사진 리스트")
