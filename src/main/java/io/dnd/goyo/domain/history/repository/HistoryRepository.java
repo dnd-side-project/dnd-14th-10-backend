b623187fe0fd593b992317f594d75840b0db5fe1
tjrwls1211 <seokjin6635@gmail.com>
2026-02-25 17:31:47 +0900

    fix: 히스토리 조회 시 삭제된 장소 필터링

--- src/main/java/io/dnd/goyo/domain/history/repository/HistoryRepository.java
+++ src/main/java/io/dnd/goyo/domain/history/repository/HistoryRepository.java
@@ -4,13 +4,15 @@ import io.dnd.goyo.domain.history.entity.History;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
 
     Optional<History> findByUserIdAndPlaceId(Long userId, Long placeId);
 
-    @EntityGraph(attributePaths = {"place", "place.placeDetail"})
-    Page<History> findAllByUserId(Long userId, Pageable pageable);
+    @Query(value = "SELECT h FROM History h JOIN FETCH h.place p JOIN FETCH p.placeDetail WHERE h.user.id = :userId AND p.status = 'ACTIVE'",
+           countQuery = "SELECT COUNT(h) FROM History h JOIN h.place p WHERE h.user.id = :userId AND p.status = 'ACTIVE'")
+    Page<History> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
 }
